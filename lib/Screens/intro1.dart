a713b1a0a52d023a8e72e5d770cd19d90c9aa524
adnanpanakkad <adnancpanakkad@gmail.com>
2023-12-23 22:18:57 +0530

    skip option in into page

--- lib/Screens/intro1.dart
+++ lib/Screens/intro1.dart
@@ -8,6 +8,7 @@ class Intro1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       backgroundColor: Colors.white,
       body: Column(
         children: [
